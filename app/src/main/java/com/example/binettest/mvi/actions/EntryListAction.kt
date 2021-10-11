bba7d90adbc861a53da593f54f7049c4af69b70c
Ivan <iaabramov_2@edu.hse.ru>
2021-10-11 23:38:10 +0300

    clear develop

--- app/src/main/java/com/example/binettest/mvi/actions/EntryListAction.kt
+++
@@ -1,5 +0,0 @@
-package com.example.binettest.mvi.actions
-
-sealed class EntryListAction {
-    object ShowClicked : EntryListAction()
-}
--- app/src/main/java/com/example/binettest/mvi/states/EntryListViewState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.binettest.mvi.states
-
-sealed class EntryListViewState {
-    object ListLoaded: EntryListViewState()
-    object ListFailed: EntryListViewState()
-}
