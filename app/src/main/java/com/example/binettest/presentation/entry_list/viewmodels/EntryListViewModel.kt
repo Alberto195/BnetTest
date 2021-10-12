0334dfd583572d6b06c2aff772185ba6fba19407
Ivan <iaabramov_2@edu.hse.ru>
2021-10-12 13:20:33 +0300

    entry data tranfer fixed

--- app/src/main/java/com/example/binettest/presentation/entry_list/viewmodels/EntryListViewModel.kt
+++ app/src/main/java/com/example/binettest/presentation/entry_list/viewmodels/EntryListViewModel.kt
@@ -1,11 +1,10 @@
 package com.example.binettest.presentation.entry_list.viewmodels
 
 import android.util.Log
-import com.example.binettest.presentation.base.viewmodel.BaseViewModel
 import com.example.binettest.domain.entry_list.models.EntryListModel
-import com.example.binettest.domain.entry_list.repositories.EntryRepository
 import com.example.binettest.domain.entry_list.usecases.GetEntryListUseCase
 import com.example.binettest.domain.entry_list.usecases.SetViewEntryUseCase
+import com.example.binettest.presentation.base.viewmodel.BaseViewModel
 import com.example.binettest.presentation.entry_list.action.EntryListAction
 import com.example.binettest.presentation.entry_list.viewstate.EntryListViewState
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -14,8 +13,8 @@ import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class EntryListViewModel(
-        private val entryListUseCase: GetEntryListUseCase,
-        private val setViewEntry: SetViewEntryUseCase
+    private val entryListUseCase: GetEntryListUseCase,
+    private val setViewEntry: SetViewEntryUseCase
 ) : BaseViewModel<EntryListAction, EntryListViewState>() {
 
     companion object {
@@ -25,7 +24,7 @@ class EntryListViewModel(
     private var entry: List<EntryListModel?>? = null
 
     override fun handleAction(action: EntryListAction) {
-        when(action) {
+        when (action) {
             is EntryListAction.ShowClicked -> {
                 getEntryList()
             }
@@ -38,7 +37,7 @@ class EntryListViewModel(
                 getObservable()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(object : DisposableObserver<List<EntryListModel?>> () {
+                    .subscribeWith(object : DisposableObserver<List<EntryListModel?>>() {
                         override fun onComplete() {
                             Log.d(TAG, "onComplete()")
                             postValue(EntryListViewState.ListLoaded)
@@ -64,7 +63,26 @@ class EntryListViewModel(
     }
 
     fun setViewEntry(entry: EntryListModel?) {
-        setViewEntry.execute(entry)
+        disposable.add(
+            Observable.defer {
+                Observable.just(setViewEntry.execute(entry))
+            }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableObserver<Unit>() {
+                    override fun onNext(t: Unit?) {
+                    }
+
+                    override fun onError(e: Throwable?) {
+                    }
+
+                    override fun onComplete() {
+
+                    }
+
+                })
+        )
+
     }
 
     private fun getObservable(): Observable<List<EntryListModel?>> {
